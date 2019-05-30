41a199235dfa93037ba1bc31af54f3bf889a03de
jiyoung <batzerohi@gmail.com>
2019-05-31 00:45:17 +0900

    003 firebase realtime database 연동 / 데이터 쓰기 기능 추가

--- app/src/main/java/com/jiyoung/doitvoca/MainActivity.java
+++ app/src/main/java/com/jiyoung/doitvoca/MainActivity.java
@@ -2,12 +2,36 @@ package com.jiyoung.doitvoca;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sendbt;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button2);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("message").push().setValue("2");
+
+            }
+
+
+        });
     }
 }
+
